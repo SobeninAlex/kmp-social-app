af7af7de8b9f5a24c9dee4b9973be2f43db5278c
SobeninAlex <sobeninalex@yandex.ru>
2025-05-27 21:19:05 +0500

    refactor arch done

--- androidApp/src/main/java/com/example/kmp_social_app/glue/data/DataModule.kt
+++ androidApp/src/main/java/com/example/kmp_social_app/glue/data/DataModule.kt
@@ -4,6 +4,8 @@ import com.example.kmp_social_app.glue.data.datasource.AccountApiDataSourceImpl
 import com.example.kmp_social_app.glue.data.datasource.AuthApiDataSourceImpl
 import com.example.kmp_social_app.glue.data.datasource.FollowsApiDataSourceImpl
 import com.example.kmp_social_app.glue.data.datasource.PostApiDataSourceImpl
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 import ru.sobeninalex.data.remote.di.ApiServiceModule
 import ru.sobeninalex.data.remote.services.account.AccountApiDataSource
@@ -13,32 +15,8 @@ import ru.sobeninalex.data.remote.services.post.PostApiDataSource
 
 val DataModule = module {
     includes(ApiServiceModule)
-
-    factory<AccountApiDataSource> {
-        AccountApiDataSourceImpl(
-            userPreferences = get(),
-            accountApiService = get()
-        )
-    }
-
-    factory<AuthApiDataSource> {
-        AuthApiDataSourceImpl(
-            userPreferences = get(),
-            authApiService = get()
-        )
-    }
-
-    factory<FollowsApiDataSource> {
-        FollowsApiDataSourceImpl(
-            userPreferences = get(),
-            followsApiService = get()
-        )
-    }
-
-    factory<PostApiDataSource> {
-        PostApiDataSourceImpl(
-            userPreferences = get(),
-            postApiService = get()
-        )
-    }
+    singleOf(::AccountApiDataSourceImpl).bind<AccountApiDataSource>()
+    singleOf(::AuthApiDataSourceImpl).bind<AuthApiDataSource>()
+    singleOf(::FollowsApiDataSourceImpl).bind<FollowsApiDataSource>()
+    singleOf(::PostApiDataSourceImpl).bind<PostApiDataSource>()
 }
